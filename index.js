171e5697b8f2bf6dd5756c98e937eb5765ae5d96
Kevin J.H Li <tensaikev@gmail.com>
2022-07-27 23:44:08 -0400

    updated files

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const SIZE = 9
 const OPERATIONS = 4
 
 const numbers = document.getElementById("numbers")
+const operations = document.getElementById("operations")
 
 function add(a,b){
     return a + b
@@ -44,14 +45,36 @@ function loadCalc(){
 
     numbers.appendChild(tempDiv)
     numbers.appendChild(decimal)
+
+    loadOperations()
 }
 
-function loadOperations{
+function loadOperations (){
     for (let i = 0; i < 4; i++){
         let newOperation = document.createElement("button")
-        
+        let val = typeOfOp(i)
+        console.log(val);
+        newOperation.setAttribute("class", `${val}`)
+        newOperation.setAttribute("id", `${val}`)
+        newOperation.innerText = val
+        operations.appendChild(newOperation)
     }
 }
+
+function typeOfOp(op){
+
+    switch(op){
+        case 0:
+            return '+'
+        case 1:
+            return '-'
+        case 2:
+            return 'x'
+        case 3:
+            return '/'
+    }
+}
+
 
 window.onload = () => {
     loadCalc()
