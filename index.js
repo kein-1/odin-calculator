b1e7928e5157d3892c9a9cdff1b777bbc037defa
Kevin J.H Li <tensaikev@gmail.com>
2022-07-28 23:44:56 -0400

    Updated files

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const SIZE = 9
 const OPERATIONS = 4
+let currVal = ""
 
 const numbers = document.getElementById("numbers")
 const operations = document.getElementById("operations")
+const equals = document.getElementById("equals")
+let calcHeaderVal = document.getElementById("results-header")
+
+equals.onclick = calculateResults
 
 function add(a,b){
     return a + b
@@ -25,6 +30,13 @@ function loadCalc(){
         let newNumb = document.createElement("button")
         newNumb.setAttribute("id", `${i}`)
         newNumb.innerText = i;
+        newNumb.addEventListener("click", () => {
+            currVal += newNumb.innerText
+            // console.log(newNumb.innerText)
+            calcHeaderVal.innerText = currVal
+            console.log(currVal)
+            return
+        })
         numbers.appendChild(newNumb)
     }
     let newNumb = document.createElement("button")
@@ -52,11 +64,18 @@ function loadCalc(){
 function loadOperations (){
     for (let i = 0; i < 4; i++){
         let newOperation = document.createElement("button")
-        let val = typeOfOp(i)
-        console.log(val);
-        newOperation.setAttribute("class", `${val}`)
-        newOperation.setAttribute("id", `${val}`)
-        newOperation.innerText = val
+        let opSymbol = typeOfOp(i)
+        newOperation.setAttribute("class", `${opSymbol}`)
+        newOperation.setAttribute("id", `${opSymbol}`)
+        newOperation.addEventListener("click", () => { 
+        currVal += opSymbol
+        // console.log(newNumb.innerText)
+        calcHeaderVal.innerText = currVal
+        console.log(currVal)
+        return
+    })
+        calcHeaderVal.innerText = currVal
+        newOperation.innerText = opSymbol
         operations.appendChild(newOperation)
     }
 }
@@ -69,11 +88,54 @@ function typeOfOp(op){
         case 1:
             return '-'
         case 2:
-            return 'x'
+            return '*'
         case 3:
             return '/'
     }
 }
+
+
+function operationsFunct(i,a,b){
+
+    switch(i){
+        case '+':
+            return add(a,b)
+        case '-':
+            return subtract(a,b)
+        case '*':
+            return multiply(a,b)
+        case '/':
+            return divide(a,b)
+    }
+}
+
+function calculateResults(){
+    if (currVal.includes('+')) {
+        let values = currVal.split("+")
+        console.log(values)
+        console.log(add(Number(values[0]), Number(values[1])))
+        return
+    }
+    if (currVal.includes('-')) {
+        let values = currVal.split("-")
+        console.log(values)
+        console.log(subtract(Number(values[0]), Number(values[1])))
+        return
+    }
+    if (currVal.includes('*')) {
+        let values = currVal.split("*")
+        console.log(values)
+        console.log(multiply(Number(values[0]), Number(values[1])))
+        return
+    }
+    if (currVal.includes('/')) {
+        let values = currVal.split("/")
+        console.log(values)
+        console.log(divide(Number(values[0]), Number(values[1])))
+        return
+    }
+}
+
 
 
 window.onload = () => {
